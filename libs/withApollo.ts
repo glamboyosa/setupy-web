299a96bfa2528c39a910cb8f2a1143b9b8a97fcd
glamboyosa <ogbemudiatimothy@gmail.com>
2020-12-15 16:28:53 +0100

    fix: removed unused locals. modified tsconfig

--- libs/withApollo.ts
+++ libs/withApollo.ts
@@ -1,15 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { createUploadLink } from 'apollo-upload-client';
 import { withApollo } from 'next-apollo';
-const link = createUploadLink({
-  uri:
-    process.env.NODE_ENV !== 'development'
-      ? 'http://localhost:8000/graphql'
-      : 'http://setupy-api.herokuapp.com/graphql',
-});
+
 const apolloClient = new ApolloClient({
   uri:
-    process.env.NODE_ENV !== 'development'
+    process.env.NODE_ENV === 'development'
       ? 'http://localhost:8000/graphql'
       : 'http://setupy-api.herokuapp.com/graphql',
   cache: new InMemoryCache(),
